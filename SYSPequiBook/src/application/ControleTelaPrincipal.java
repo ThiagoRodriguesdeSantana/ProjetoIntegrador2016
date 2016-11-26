ce1cc632f99915a1f1320e0e6627daff75d0d027
ThiagoSantana <thiagorodriguescamra@gmail.com>
2016-11-25 22:00:05 -0200

    Ajustando tela

--- SYSPequiBook/src/application/ControleTelaPrincipal.java
+++ SYSPequiBook/src/application/ControleTelaPrincipal.java
@@ -1,30 +1,58 @@
 package application;
 
+import java.awt.Image;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javax.swing.text.html.ImageView;
+
 import entidade.EMensagem;
-import entidade.EPerfilUsuario;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import negocio.NAcesso;
 import negocio.NMensagem;
 
 public class ControleTelaPrincipal implements Initializable{
 	
+	
 	@FXML
-	private Label nome;
+	private AnchorPane panePrincipal;
 	@FXML
-	private TableView<TabelaMensagem> mensagem;
+	private Pane pane1;
+	@FXML
+	private Button acessoPerfil;
+	@FXML
+	private Button fotos;
+	@FXML
+	private Button btnAddPost;
+	@FXML
+	private Button btnRemovePost;
+	@FXML
+	private Button btnSair;
+	@FXML
+	private ImageView imgTimaLine;
+	@FXML
+	private TableView<TabelaMensagem> tblSolicitacoes;
+	@FXML
+	private TableColumn<TabelaMensagem, String> colUsuarios;
+	@FXML
+	private Label nomeUsuario;
+	@FXML
+	private ComboBox<PopulaComboPesquisaAmigos> cboPesquisaAmigos;
+	@FXML
+	private TableView<TabelaMensagem> tblMensagem;
 	@FXML
 	private TableColumn<TabelaMensagem, String> texto;
 	
@@ -36,7 +64,8 @@ public class ControleTelaPrincipal implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		
+		
 		
 	}
 	
@@ -60,8 +89,30 @@ public class ControleTelaPrincipal implements Initializable{
 		
 		texto.setCellValueFactory(new PropertyValueFactory<TabelaMensagem, String>("Mensagem"));
 		
+		tblMensagem.setItems(listMensage);
+		
 	}
-	
+	@FXML
+	private void AcessarPerfil(ActionEvent event){
+		
+		
+	}
+	@FXML
+	private void VerFotos(ActionEvent event){
+		
+	}
+	@FXML
+	private void AdicionarPstagem(ActionEvent event){
+		
+	}
+	@FXML
+	private void RemoverPstagem(ActionEvent event){
+		
+	}
+	@FXML
+	private void Sair(ActionEvent event){
+		
+	}
 	
     
      
